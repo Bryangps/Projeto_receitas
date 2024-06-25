165a2140557350b49db0f71fdd71ca411812a1af
Bryangps <bryan.gps18@gmail.com>
2024-06-25 14:00:04 -0300

    Foi colocado url dinamica na pagina home, e foi criado o app_name, e foi adicionado dados ficticio em recipe

--- recipe/views.py
+++ recipe/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from utils.recipe.factory import make_recipe
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html',
+                  {'recipes': [make_recipe for _ in range(8)]})
 
 
 def recipe(request, pk):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html',
+                  {'recipe': make_recipe,
+                   'is_detail_page': True})
 
